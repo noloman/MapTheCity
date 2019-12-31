373bbc8cca2dacac9b455e207db0943aba580ced
Manuel Lorenzo <manulorenzop@gmail.com>
2019-12-31 18:47:58 +0100

    add class comment

--- app/src/main/java/me/manulorenzo/citymaps/data/source/Mockable.kt
+++ app/src/main/java/me/manulorenzo/citymaps/data/source/Mockable.kt
@@ -1,3 +1,6 @@
 package me.manulorenzo.citymaps.data.source
 
+/**
+ * Annotation class to help us mock Kotlin classes as they are closed by default along with the allOpen plugin
+ */
 annotation class Mockable
