d75ab8cbb8ad71e30a6529459b172c7407caf413
Manuel Lorenzo <manulorenzop@gmail.com>
2020-01-01 21:19:46 +0100

    improve resource class design
    no need to pass data for Loading or Error

--- app/src/main/java/me/manulorenzo/citymaps/data/Resource.kt
+++ app/src/main/java/me/manulorenzo/citymaps/data/Resource.kt
@@ -8,6 +8,6 @@ sealed class Resource<T>(
     val message: String? = null
 ) {
     class Success<T>(data: T) : Resource<T>(data)
-    class Loading<T>(data: T? = null) : Resource<T>(data)
-    class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
+    class Loading<T> : Resource<T>(null)
+    class Error<T>(message: String) : Resource<T>(null, message)
 }
